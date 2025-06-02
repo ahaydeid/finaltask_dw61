d753c3e4ef431f40485cf32bc1924d5f0a93dcb9
ahaydeid <adi.hadi270@gmail.com>
2025-06-02 20:30:47 +0700

    Senin, 2 juni 2025 (Sesi 2): Update tambah HBS untuk login, register, dan dashboard

---
+++ src/assets/script.js
@@ -0,0 +1,11 @@
+const toggleButtons = document.querySelectorAll(".togglePassword");
+
+toggleButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const targetId = button.getAttribute("data-target");
+    const input = document.getElementById(targetId);
+    const type = input.getAttribute("type") === "password" ? "text" : "password";
+    input.setAttribute("type", type);
+    button.textContent = type === "password" ? "🙈" : "👁️";
+  });
+});

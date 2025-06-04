fb52704a1628c9726d40a20bcee3a019eded79b5
ahaydeid <adi.hadi270@gmail.com>
2025-06-04 12:15:53 +0700

    update

--- controllers/experienceController.js
+++ controllers/experienceController.js
@@ -1,6 +1,9 @@
-export function manageExperience(req, res) {
-  res.render("manageexperience");
-}
+import db from "../model/connection.js";
+
+export const manageExperience = async (req, res) => {
+  const data = await db.query("SELECT * FROM experience ORDER BY id DESC");
+  res.render("manageexperience", { hasil: data.rows });
+};
 
 export function addExperience(req, res) {
   res.render("addexperience");
--- routes/route.js
+++ routes/route.js
@@ -14,8 +14,13 @@ router.get("/", home);
 router.get("/login", login);
 router.get("/register", register);
 router.post("/register", handleRegister);
+
+// Sementara untuk keperluan desain UI nya dulu
+router.get("/dashboard", dashboard);
+
+// MANAGE TECH STACK
 router.get("/managestack", manageStack);
-// Upload stack-icon dan routernya
+// Multer untuk tech stack (masuk ke folder src/assets/tech)
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // cb(null, "uploads/stack");
@@ -34,15 +39,23 @@ router.get("/editstack/:id", editStack);
 router.post("/update-stack", upload.single("stack-icon"), updateStack);
 router.post("/delete/:id", deleteStack);
 
-// router.post("/editstack");
-
+// MANAGE EXPERIENCE
 router.get("/manageexperience", manageExperience);
+// Multer untuk tech stack (masuk ke folder uploads/experience)
+const storage2 = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // cb(null, "uploads/stack");
+    cb(null, "uploads/experience");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const upload2 = multer({ storage: storage2 });
 router.get("/addexperience", addExperience);
 
+// MANAGE PROJECT
 router.get("/manageproject", manageProject);
 router.get("/addproject", addProject);
-
-// Sementara untuk keperluan desain UI nya dulu
-router.get("/dashboard", dashboard);
 
 export default router;

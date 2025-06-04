e2b335d4a79e63787fbe9e46319c1dd0e9a4f013
ahaydeid <adi.hadi270@gmail.com>
2025-06-05 03:31:45 +0700

    Kamis, 5 Juni 2025 (Sesi 2): "Hari ini sampai sini dulu, niat mau tidur awal karena besok presentasi, malah begadang karena susah tidur kepikiran besok"

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,9 +1,16 @@
 import db from "../model/connection.js";
 
-export const dashboard = (req, res) => {
-  // const data = await db.query("SELECT * FROM public.user");
+export const dashboard = async (req, res) => {
   const userData = req.session.user; // isinya: name, email, foto
+  const dataStack = await db.query("SELECT * FROM techstack");
+  const dataExperience = await db.query("SELECT * FROM experience");
+  const dataProject = await db.query("SELECT * FROM project");
 
   // res.render("dashboard", userData);
-  res.render("dashboard", { user: userData });
+  res.render("dashboard", {
+    user: userData,
+    stack: dataStack.rows.length,
+    experience: dataExperience.rows.length,
+    project: dataProject.rows.length,
+  });
 };
